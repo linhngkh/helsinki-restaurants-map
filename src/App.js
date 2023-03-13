f61f6602f3a0f6dbe71438a85c3ec96b40b86313
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-03-13 17:23:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, Fragment } from "react";
+import data from "./data.json";
+import styled from "styled-components";
+import Navbar from "./components/Navbar";
+
+const Section = styled.div`
+  width: 100%;
+  padding: 1rem 4rem;
+`;
+
+const ImageContainers = styled.div`
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 10px 15px;
+`;
+
+const ImageBox = styled.div`
+  padding-bottom: 20px;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 300px;
+  border-radius: 5px;
+  object-fit: cover;
+  object-position: bottom;
+`;
+
+const Title = styled.h2`
+  margin-bottom: 7px;
+`;
+const Desc = styled.h3`
+  margin-bottom: 10px;
+`;
+const Tags = styled.p`
+  font-size: 14px;
+  background-color: #00c2eb;
+  padding: 10px;
+  width: 5rem;
+  display: inline;
+  border-radius: 5px;
+  color: white;
+  text-transform: capitalize;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Navbar />
+      <Section>
+        <ImageContainers>
+          {" "}
+          {data.restaurants.map((item, id) => (
+            <ImageBox key={id}>
+              <Image src={item.image} alt="" />
+
+              <Title>{item.name}</Title>
+              <Desc>{item.description}</Desc>
+              <Tags>{item.tags}</Tags>
+            </ImageBox>
+          ))}
+        </ImageContainers>
+      </Section>
+    </Fragment>
   );
 }
 

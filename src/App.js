c7082ab3eb4f8df0a553c6fce5df4644789cbc5d
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-03-13 23:35:24 +0200

    useContext setup

--- src/App.js
+++ src/App.js
@@ -1,74 +1,12 @@
 import { Fragment } from "react";
-import data from "./data.json";
-import styled from "styled-components";
-import Navbar from "./components/Navbar";
-
-const Section = styled.div`
-  width: 100%;
-  padding: 1rem 4rem;
-`;
-
-const ImageContainers = styled.div`
-  display: grid;
-  grid-template-rows: auto;
-  grid-template-columns: 1fr 1fr 1fr;
-  gap: 20px 40px;
-`;
-
-const ImageBox = styled.div`
-  padding-bottom: 20px;
-`;
-
-const Image = styled.img`
-  width: 100%;
-  height: 300px;
-  border-radius: 10px;
-  object-fit: cover;
-  object-position: bottom;
-  -webkit-transition: 0.3s ease-in;
-  transition: 0.3s ease-in;
-  &:hover {
-    -webkit-transform: scale(1.2);
-    transform: scale(1.2);
-  }
-`;
-
-const Title = styled.h2`
-  margin-bottom: 7px;
-`;
-const Desc = styled.h3`
-  margin-bottom: 10px;
-`;
-const Tags = styled.p`
-  font-size: 14px;
-  background-color: black;
-  padding: 10px;
-  width: 5rem;
-  display: inline;
-  border-radius: 5px;
-  color: white;
-  text-transform: capitalize;
-`;
+import Navbar from "./components/navbar/Navbar";
+import Restaurant from "./components/restaurants/Restaurant";
 
 function App() {
-  console.log(data.restaurants);
   return (
     <Fragment>
-      <Navbar data={data} />
-      <Section>
-        <ImageContainers>
-          {" "}
-          {data.restaurants.map((item, id) => (
-            <ImageBox key={id}>
-              <Image src={item.image} alt="photo" />
-
-              <Title>{item.name}</Title>
-              <Desc>{item.description}</Desc>
-              <Tags>{item.tags}</Tags>
-            </ImageBox>
-          ))}
-        </ImageContainers>
-      </Section>
+      <Navbar />
+      <Restaurant />
     </Fragment>
   );
 }

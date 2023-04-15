07b4c29fc52b1f05118a7bc0701fb60dd7594b00
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-15 19:29:35 +0300

    shown markers on map

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,28 +1,60 @@
-import React, { useState, useRef, useCallback } from "react";
-import MapBox from "react-map-gl";
+import React, { useState, useRef } from "react";
+import MapBox, { Marker, Popup } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { useGlobalContext } from "../../context/SortingContext";
 
 const TOKEN = process.env.REACT_APP_TOKEN;
 const Map = () => {
+  const { sortingList } = useGlobalContext();
+  // set up map
   const [viewport, setViewport] = useState({
-    latitude: 37.8,
-    longitude: -122.4,
+    longitude: 24.941325187683105,
+    latitude: 60.169938852212965,
+    zoom: 16,
+    transitionDuration: "200",
   });
-  const mapRef = useRef();
-  const handleViewportChange = useCallback(
-    (newViewport) => setViewport(newViewport),
-    []
-  );
+  const [selectedPlace, setSelectedPlace] = useState(null);
+
   return (
     <div style={{ width: "100%", height: 400 }}>
       <MapBox
         {...viewport}
+        maxZoom={20}
         mapboxAccessToken={TOKEN}
-        initialViewState={{
-          zoom: 14,
-        }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
-      ></MapBox>
+        onViewportChange={(newViewport) => setViewport(newViewport)}
+      >
+        {/* markers */}
+        {sortingList.map((place, id) => (
+          <Marker
+            key={id}
+            latitude={place.location[1]}
+            longitude={place.location[0]}
+          >
+            <button
+              style={{ cursor: "pointer", background: "none", border: "none" }}
+              onClick={(e) => {
+                e.preventDefault();
+                setSelectedPlace(place);
+              }}
+            >
+              <img
+                src="/marker-flags-svgrepo-com.svg"
+                alt="restaurant"
+                style={{ width: "20px", height: "20px" }}
+              />
+            </button>
+          </Marker>
+        ))}
+        {/* (selectedPlace ? (
+        <Popup
+          latitude={selectedPlace.location[1]}
+          longitude={selectedPlace.location[0]}
+        >
+          <div>restaurant</div>
+        </Popup>{" "}
+        ): null) */}
+      </MapBox>
     </div>
   );
 };

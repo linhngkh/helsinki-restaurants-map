417c8ef4eac04643b02d236264b362cd695cc984
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-05-11 12:21:12 +0300

    make logic for booking time slots

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactCalendar from "react-calendar";
-import { Container } from "./Calendar.styles";
+import { Container, RenderCalendar } from "./Calendar.styles";
 import "react-calendar/dist/Calendar.css";
+import { add } from "date-fns";
 
 const Calendar = () => {
+  const [date, setDate] = useState({
+    justDate: null,
+    dateTime: null,
+  });
+
+  const getTimes = () => {
+    // if date is not exist
+    if (!date.justDate) {
+      return;
+    }
+    // destructure initial date value object
+    const { justDate } = date;
+    //config time restaurant open and close and the time we book beforehand
+    const beginning = add(justDate, { hours: 10 });
+    const end = add(justDate, { hours: 20 });
+    //   and the time we book beforehand in minutes(a day)
+    const interval = 1440;
+
+    // make empty array for times
+    const times = [];
+    for (let i = beginning; i <= end; i = add(i, { minutes: interval })) {
+      // push i element in times array
+      times.push(i);
+    }
+    return times;
+  };
+
+  const times = getTimes();
+
   return (
     <Container>
-      <ReactCalendar
-        minDate={new Date()}
-        className="REACT-CALENDAR"
-        view="month"
-        onClickDay={(date) => console.log(date)}
-      />
+      {date.justDate ? (
+        <RenderCalendar></RenderCalendar>
+      ) : (
+        <ReactCalendar
+          minDate={new Date()}
+          className="REACT-CALENDAR"
+          view="month"
+          onClickDay={(date) =>
+            setDate((prev) => ({ ...prev, justDate: date }))
+          }
+        />
+      )}
     </Container>
   );
 };
--- src/components/calendar/Calendar.styles.jsx
+++ src/components/calendar/Calendar.styles.jsx
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 export const Container = styled.div`
-  width: 300px;
+  width: 500px;
   display: flex;
   justify-content: center;
   margin: 0 auto;
   align-items: center;
   min-height: 100vh;
+`;
+
+export const RenderCalendar = styled.div`
+  display: flex;
+  gap: 4;
 `;
